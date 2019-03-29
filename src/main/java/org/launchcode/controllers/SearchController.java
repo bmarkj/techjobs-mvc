7f6ad16110c6ca85eb05cc1f6dd7f6d1b3f89ff2
Mark Brown <mbrown@gmx.us>
2019-03-28 19:56:21 -0500

    Fixed bug when searching "all" for a specific search term

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -25,9 +25,18 @@ public class SearchController {
     // TODO #1 - Create handler to process search request and display results
     @RequestMapping(value="results")
     public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobCount", jobs.size() + " Result(s)");
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("jobs", jobs); //place search results in model so views can retrieve it
+
+            return "search";
+        }
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         model.addAttribute("jobCount", jobs.size() + " Result(s)");
-        //model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", ListController.columnChoices);
         model.addAttribute("jobs", jobs); //place search results in model so views can retrieve it
 
         return "search"; //this is an HTML file, you just don't add teh extension to it
